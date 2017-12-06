60cc99196ddd4880dd536aa01bdd3b3b9c4a47da
JinerHuang <495470516@qq.com>
2017-12-06 16:13:00 +0800

    Update CodeValueEnum.java

--- WorkCode/Enum/CodeValueEnum.java
+++ WorkCode/Enum/CodeValueEnum.java
@@ -1,1 +1,16 @@
 
+import java.util.Map;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+
+@FunctionalInterface
+public interface CodeValueEnum {
+   static <T extends CodeValueEnum> Map <Integer, T> buildMapping(T[] types){
+       Builder<Integer, T> builder = ImmutableMap.<Integer, T> builder();
+       for(T type : types){
+           builder.put(type.getCode(), type);
+       }
+       return builder.build();
+   }
+   int getCode();
+}
