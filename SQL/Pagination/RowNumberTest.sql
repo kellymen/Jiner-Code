c89dd07a76e1070875d6a516d0a670b891b01368
JinerHuang <495470516@qq.com>
2017-12-07 09:59:31 +0800

    Create RowNumberTest.sql

---
+++ SQL/Pagination/RowNumberTest.sql
@@ -0,0 +1,15 @@
+
+dbcc freeproccache
+dbcc dropcleanbuffers
+set statistics time on
+set statistics io on
+set statistics profile on;
+ 
+with #pager as 
+(
+select ID,Title,ROW_NUMBER() OVER(Order By ID) as rowid from Article_Detail 
+)
+select ID,Title from #pager where rowid between (15 * (50-1)+1) and 15 * 50
+ 
+ 
+set statistics profile off;
