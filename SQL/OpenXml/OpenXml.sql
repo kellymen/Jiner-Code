e222b450f09df50aadcdb889d44e97f0373c1f5d
JinerHuang <495470516@qq.com>
2017-12-27 11:32:44 +0800

    Update OpenXml.sql

--- SQL/OpenXml/OpenXml.sql
+++ SQL/OpenXml/OpenXml.sql
@@ -3,19 +3,30 @@ BEGIN RTY
   DECLARE @xml xml
   
   SET @xml = '<?xml version="1.0"?>
+  <root>
   <ArrayOfInt>
     <int>1</int>
     <int>2</int>
     <int>3</int>
     <int>4</int>
     <int>5</int>
-  </ArrayOfInt>'
+  </ArrayOfInt>
+  <ArrayOfLong>
+    <int>6</int>
+    <int>7</int>
+    <int>8</int>
+    <int>9</int>
+    <int>10</int>
+  </ArrayOfLong>
+  </root>'
   
   EXEC sp_xml_preparedocument @idoc OUTPUT, @xml
   
-  select * from OPENXML(@idoc, '/ArrayOfInt', 2) as T where T.text is not null
+  select * from OPENXML(@idoc, '/root/ArrayOfInt/int') with ([int] int '.') --good
   
-  select * from OPENXML(@idoc, '/ArrayOfInt', 3) where nodetype = 3
+  select * from OPENXML(@idoc, '/root/ArrayOfInt', 2) as T where T.text is not null
+  
+  select * from OPENXML(@idoc, '/root/ArrayOfInt', 3) where nodetype = 3
   
   EXEC sp_xml_removedocument @idoc
   
