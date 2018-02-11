1b2b8ae261a7e2e4b294951148e2ad5cfb27ba5e
JinerHuang <495470516@qq.com>
2018-02-11 16:01:46 +0800

    Create umeditor_contentchange.js

---
+++ JavaScript/umeditor_contentchange.js
@@ -0,0 +1,27 @@
+me.addListener('contentchange', function(){
+
+        /*if ( !saveKey ) {
+            return;
+        }
+
+        if ( me._saveFlag ) {
+            window.clearTimeout( me._saveFlag );
+        }
+
+        if ( me.options.saveInterval > 0 ) {
+
+            me._saveFlag = window.setTimeout( function () {
+
+                save( me );
+
+            }, me.options.saveInterval );
+
+        } else {
+
+            save(me);
+
+        }*/
+		//alert((me.getPlainTxt().replace(/\s+/g, "")).length);  //数字统计
+		//alert(me.getPlainTxt().indexOf(" ") == -1); //空格判断， true 不存在；false 存在
+
+    })
